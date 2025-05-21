5274ebd3330d551079fa3b8e2080fcd19ff3f98a
Farshad-afk <farshad.fakhrezy@gmail.com>
2025-05-21 15:10:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.title("-bgshad")
 st.write("💲🎰 bukan adiksi tapi dedikasi🤑😈🙏🏿.")
 st.image("05_39_10_Gdillk_WMAAtPqx.jpg", width=200)
 
-st.title("Situs Gacor 777")
+st.title("Gacor 777🎰")
 st.header("Mengecek Angka")
 angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
 
 if (angka % 2) == 0:
-  st.write(f"{angka} Jackpot")
+  st.write(f"{angka} Kamu Jackpot🤑")
 else:
-  st.write(f"{angka} Rugi Bandar")
+  st.write(f"{angka} Rugi Bandar🙏🏿😈")
