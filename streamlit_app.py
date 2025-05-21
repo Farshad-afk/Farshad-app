c3dd7b8bffb4a0f94a2480d840d3b1075d0f0cde
Farshad-afk <farshad.fakhrezy@gmail.com>
2025-05-21 15:03:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title("-bgshad")
 st.write("💲🎰 bukan adiksi tapi dedikasi🤑😈🙏🏿.")
 st.image("05_39_10_Gdillk_WMAAtPqx.jpg", width=200)
+
+st.title("Aplikasi Sederhana")
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+  st.write(f"{angka} adalah Bilangan Genap")
+else:
+  st.write(f"{angka} adalah Bilangan Ganjil")
