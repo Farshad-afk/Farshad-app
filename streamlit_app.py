a010f7a01f25d7baaa82a1ced6b8c534b30fa1b7
Farshad-afk <farshad.fakhrezy@gmail.com>
2025-05-21 14:51:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
 st.title("-Farshad")
-st.write(
-    "💲🎰 bukan adiksi tapi dedikasi🤑😈🙏🏿.")
-
-st.image("05_39_10_Gdillk_WMAAtPqx.jpg",width=200)
+st.write("💲🎰 bukan adiksi tapi dedikasi🤑😈🙏🏿.")
+st.image("05_39_10_Gdillk_WMAAtPqx.jpg", width=200)
